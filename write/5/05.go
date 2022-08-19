3e58bb447500c2a381603da41e4a799c5fa00635
Ccaswell42 <dmitriykuzin11@yandex.ru>
2022-08-19 16:27:56 +0300

    add 5

---
+++ write/5/05.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+// фнукция wtiter в бесконечном цикле  пишет значения в канал, как только сработает таймер контекста -
+// закроет канал и выйдет из функции
+func writer(ctx context.Context, ch chan int) {
+	i := 0
+	for {
+		select {
+		case <-ctx.Done():
+			close(ch)
+			return
+		default:
+			ch <- i
+		}
+		time.Sleep(100 * time.Millisecond) // <- для наглядности
+		i++
+	}
+
+}
+func main() {
+
+	// количество секунд, после которых, программа завершится возьмем из аргумента коммандной строки
+	sec, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	// устанавливаем контекст с таймаутом
+	ctx, _ := context.WithTimeout(context.Background(), time.Duration(sec)*time.Second)
+
+	// создаем канал куда будем писать и откуда читать
+	ch := make(chan int, 1)
+	// запускаем в отдельной горутине наше чтение
+	go writer(ctx, ch)
+	// читаем пока есть что читать, и пока канал не закрыт
+	for val := range ch {
+		fmt.Println(val)
+	}
+	// как только сработает таймаут, запись прекратится, чтение тоже, и мы заврешим работу нашей прогрмамы
+	fmt.Println("bye bye")
+
+}
