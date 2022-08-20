bcfeae38593a80303d6e935fcd7b387d906f26a7
Ccaswell42 <dmitriykuzin11@yandex.ru>
2022-08-20 19:36:44 +0300

    add 14

---
+++ write/14/14.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// fmt работает через рефлексию. Рефлексия - всегда рантайм.
+
+//Функция принимает пустой интерфейс и через форматированный вывод (:T) пишет в консоль ее тип.
+func printType(val interface{}) {
+	fmt.Printf("Тип данных: %T\n", val)
+}
+func main() {
+	var a int
+	var b string
+	var c bool
+	d := make(chan struct{})
+	printType(a)
+	printType(b)
+	printType(c)
+	printType(d)
+}
