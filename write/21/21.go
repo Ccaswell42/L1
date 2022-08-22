abeb62850179fce833c86c9c83b5d93032bc07ad
Ccaswell42 <dmitriykuzin11@yandex.ru>
2022-08-22 09:25:40 +0300

    comment 21

--- write/21/21.go
+++ write/21/21.go
@@ -1,20 +1,25 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
-// есть 2 структуры: старая и новая. у старой есть какие то методы
-// есть интерфейс который реализует новая структура. чтобы этот интерфейс реализовал старую струтуру
-// нужно сделать адаптер который унаследует старую структуру и будет иметь метод который будет соответствовать данному
-// интерфейсу.
+// Идея паттерна "адаптер" заключается в том, что для того чтобы уже существующая структура и ее методы удовлетворяли
+// интерфейсу не нужно переписывать структуру или методы, а нужно сделать для нее адаптер.
 
+// Есть структура Wallet для которой мы сделаем адаптер
 type Wallet struct {
 	cash int
 }
 
+// Структура Card
 type Card struct {
 	balance int
 }
 
+// Метод Pay для структуры Card
 func (c *Card) Pay(amount int) error {
 	if amount > c.balance {
 		return errors.New("не хватает денег на карте")
@@ -23,18 +28,42 @@ func (c *Card) Pay(amount int) error {
 	return nil
 }
 
+// есть интерфейс Payyer, которому удовлетворяет только структура Card
 type Payer interface {
 	Pay(int) error
 }
 
+// Для того чтобы интерфейсу Payer удовлетворяла и структура Wallet сделаем новую структуру WalletAdapter,
+// которая унаследует все от структуры Wallet + будет иметь свой собственный метод Pay, который будет удовлетворять
+// нашему интерфейсу Payer
 type WalletAdapter struct {
-	Wallet
+	*Wallet
 }
 
+// Метод Pay структуры WallettAfapter удовлетворяет интерфейсу Payer
 func (w *WalletAdapter) Pay(amount int) error {
 	if amount > w.cash {
 		return errors.New("не хватает денег в кошельке")
 	}
 	w.cash -= amount
 	return nil
+}
+
+// Функция Buy принимает интерфейс Payer;  Списывает деньги с карты или с кошелька;
+// Dыдает ошибку, если денег не достаточно
+func Buy(p Payer) {
+	if err := p.Pay(100); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Покупка совершена упешно с помощью %T\n", p)
+}
+
+func main() {
+	//инициализируем наши стуктуры
+	myWallet := &WalletAdapter{&Wallet{cash: 1000}}
+	myCard := &Card{1000}
+
+	// используем функцию Buy для обоих объектов
+	Buy(myCard)
+	Buy(myWallet)
 }
